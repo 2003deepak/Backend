32558bad67e5f320c247dd223e660733669ac387
Deepak Kumar Yadav <98113881+2003deepak@users.noreply.github.com>
2024-07-19 12:52:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://frontend-roan-five-41.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
